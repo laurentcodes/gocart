00a0bfdf3e0208fe959ccf3cd98c82935cf9c57d
Laurent <o.sherkes@gmail.com>
2022-06-01 14:15:07 +0100

    fix: minor fix to User Screen

--- src/screens/Users/ListItem.jsx
+++ src/screens/Users/ListItem.jsx
@@ -6,17 +6,17 @@ import { formatISO } from '../../util/date';
 const ListItem = ({ item }) => {
 	return (
 		<tr>
-			<td style={{ width: '30%' }}>
+			<td style={{ width: '35%' }}>
 				<FlexWrap>
 					<StyledLink to={`/users/${item.user_id}`}>
 						<p>{item.user_id}</p>
 					</StyledLink>
 				</FlexWrap>
 			</td>
-			<td style={{ width: '35%' }}>
+			<td style={{ width: '45%' }}>
 				<p>{item.username}</p>
 			</td>
-			<td style={{ width: '35%' }}>{formatISO(item.createdAt)}</td>
+			<td style={{ width: '20%' }}>{formatISO(item.createdAt)}</td>
 		</tr>
 	);
 };
